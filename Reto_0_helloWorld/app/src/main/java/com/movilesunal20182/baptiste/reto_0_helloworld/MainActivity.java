380ee2cc025f5fc98f86c22db91cb5a4e137e5ce
BaptisteViry <baptiste.viry@orange.fr>
2018-08-19 15:17:42 -0500

    Reto_0_helloWorld

---
+++ Reto_0_helloWorld/app/src/main/java/com/movilesunal20182/baptiste/reto_0_helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.movilesunal20182.baptiste.reto_0_helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
